29d0bf592b6d9420ba7722dff5b6a0f08d4ceecf
Roger Pettett <roger.pettett@nanoporetech.com>
2017-11-03 10:49:00 +0000

    updated changelog & version

--- lib/ClearPress/view.pm
+++ lib/ClearPress/view.pm
@@ -24,7 +24,7 @@ use MIME::Base64 qw(encode_base64);
 use JSON;
 use Readonly;
 
-our $VERSION = q[477.1.3];
+our $VERSION = q[477.1.4];
 
 our $DEBUG_OUTPUT        = 0;
 our $DEBUG_L10N          = 0;
